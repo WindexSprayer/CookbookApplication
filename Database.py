f2f8021d247a1b56bb51f2db99b1d02e1c3ceed5
GlasgowK2561 <144264822+GlasgowK2561@users.noreply.github.com>
2023-12-03 15:48:57 -0600

    Add files via upload

--- Database.py
+++ Database.py
@@ -27,7 +27,7 @@ import re
 global ingredientsdf, recipedf, stepsdf
 filename = 'cookbook.xlsx' #filename
 #column namesfor our dataframes
-columnsRecipes = ['RecipeID', 'RecipeName', 'Description', 'tags', 'foodCat', 'cuisine', 'prepTime','cookTime', 'servings','numIngred','numSteps']
+columnsRecipes = ['RecipeID', 'RecipeName', 'Description', 'tags', 'foodCat', 'cuisine', 'prepTime','cookTime', 'servings']
 columnsIngredients = ['IngredListID', 'Name', 'Amount', 'Unit']
 columnsSteps = ['StepListID', 'StepNum', 'Instruction']
 
@@ -38,7 +38,9 @@ def open_cookbook():
         ingredientsdf = pd.read_excel(filename, sheet_name='Ingredients') #open the sheet in the file named 'ingredients' to get the ingredientsdf
         recipedf = pd.read_excel(filename, sheet_name='Recipe') #open the sheet in the file named 'recipe' to get the recipedf
         stepsdf = pd.read_excel(filename, sheet_name='Steps') #open the sheet in the file named 'steps' to get the stepsdf
-       
+        ingredientsdf = ingredientsdf.fillna("")
+        recipedf = recipedf.fillna("")
+        stepsdf = stepsdf.fillna("")
     except FileNotFoundError:
         #if the file does not exist, then create the dataframes, then save it
         ingredientsdf = pd.DataFrame(columns = columnsIngredients) #create a dataframe for ingredients, recipes, steps
@@ -64,6 +66,9 @@ def save_dfs():
     ingredientsdf = pd.read_excel(filename, sheet_name='Ingredients')
     recipedf = pd.read_excel(filename, sheet_name='Recipe')
     stepsdf = pd.read_excel(filename, sheet_name='Steps')
+    ingredientsdf = ingredientsdf.fillna("")
+    recipedf = recipedf.fillna("")
+    stepsdf = stepsdf.fillna("")
     return #end of save_dfs function
 
 #this function will be called when displaying the dropdown menu of recipes for the user to select
@@ -75,7 +80,8 @@ def getRecipeInformation(recipeName):
     #get the recipe associated with the  recipe name
     global ingredientsdf, recipedf, stepsdf, filename #use global variables
     recipe = recipedf.loc[recipedf['RecipeName'] == recipeName] #make a df with just the recipe we are searching for
-    recipeID = recipe.iloc[0]['RecipeID']  #find the recipeID--this will find the recipes ingredients and steps
+    idNums = recipe['RecipeID'].to_list() #get the id number
+    recipeID = idNums[-1] #find the recipeID--this will find the recipes ingredients and steps
     recipe = recipe.drop(columns=['RecipeID']) #drop the recipeID column 
     recipe = recipe.to_dict('records') 
     recipeInformation = dict(recipe[0]) #recipedf is now a dictionary
@@ -102,10 +108,15 @@ def addRecipe(recipeInformation):
     del recipeInformation['instructions']
 
     recipe = recipeInformation #get the recipe information from the dictionary (what is left of the dictionary)
-    listID = len(recipedf) + 1 #create the unique id num for the recipe information
-
-    recipeID = {'RecipeID': len(recipedf) + 1} 
+    try:
+        last_recipe = recipedf.loc[len(recipedf)-1]
+        last_id = last_recipe['RecipeID']
+        listID = last_id + 1 #create the unique id num for the recipe information
+    except KeyError:
+        listID = len(recipedf) + 1
+    recipeID = {'RecipeID': listID} 
     recipe.update(recipeID) #add the id num to the recipe information
+
     recipedf.loc[len(recipedf) + 1] = recipe #save to recipedf
     #update the ingredients information to add the unique id
     for item in ingredients:
@@ -126,7 +137,8 @@ def addRecipe(recipeInformation):
 def deleteRecipe(recipeName):
     global ingredientsdf, recipedf, stepsdf, filename #use global variables
     recipe = recipedf.loc[recipedf['RecipeName'] == recipeName]  #find the recipe we are removing
-    id = recipe.iloc[0]['RecipeID'] #get the id number
+    idNums = recipe['RecipeID'].to_list() #get the id number
+    id = idNums[-1] #find the existing ID number, so we can replace the recipe information
     recipedf = recipedf.drop(recipedf[recipedf['RecipeName'] == recipeName].index) #delete the recipe
     ingredientsdf = ingredientsdf.drop(ingredientsdf[ingredientsdf['IngredListID'] == id].index) #delete the ingredients
     stepsdf = stepsdf.drop(stepsdf[stepsdf['StepListID'] == id].index) #delete the instructions
@@ -136,7 +148,8 @@ def deleteRecipe(recipeName):
 def editRecipe(recipeInformation, recipeName):
     global ingredientsdf, recipedf, stepsdf, filename #use global variables
     recipe = recipedf.loc[recipedf['RecipeName'] == recipeName] #find the existing recipe
-    id = recipe.iloc[0]['RecipeID'] #find the existing ID number, so we can replace the recipe information
+    idNums = recipe['RecipeID'].to_list() #get the id number
+    id = idNums[-1] #find the existing ID number, so we can replace the recipe information
     recipeInformation.update({'RecipeID': id} ) #update the given information to use the previous recipeID -- allowing us to keep 
                                                 #the same ingredients and steps
     recipedf = recipedf.drop(recipedf[recipedf['RecipeName'] == recipeName].index) #delete the old information
@@ -147,15 +160,16 @@ def editRecipe(recipeInformation, recipeName):
 def editSteps(newSteps, recipeName):
     global ingredientsdf, recipedf, stepsdf, filename
     recipe = recipedf.loc[recipedf['RecipeName'] == recipeName]  #find the existing recipe id number
-    id = recipe.iloc[0]['RecipeID'] #find the id number
+    idNums = recipe['RecipeID'].to_list() #get the id number
+    id = idNums[-1] #find the id number
     stepsdf = stepsdf.drop(stepsdf[stepsdf['StepListID'] == id].index) #delete the old steps
 
     itemNum = 1
-    steps = [] #create a list, so we can create a list of dictionaries containing the step information
+    steps = []
     for item in newSteps:
         steps.append( {'StepListID':id,'StepNum': itemNum,'Instruction': item})
         itemNum += 1
-    newStepsdf = pd.DataFrame(columns = columnsSteps, data = steps) #create a df from the list of dictionaries with new steps
+    newStepsdf = pd.DataFrame(columns = columnsSteps, data = steps) 
     stepsdf = pd.concat([stepsdf, newStepsdf], ignore_index = True)#save to the stepsdf
     save_dfs()
     return
@@ -163,14 +177,51 @@ def editSteps(newSteps, recipeName):
 def editIngredients(newIngredients, recipeName):
     global ingredientsdf, recipedf, stepsdf, filename
     recipe = recipedf.loc[recipedf['RecipeName'] == recipeName]  #find the existing recipe id number
-    id = recipe.iloc[0]['RecipeID'] #find the id number
+    idNums = recipe['RecipeID'].to_list() #get the id number
+    id = idNums[-1]#find the id number
     ingredientsdf = ingredientsdf.drop(ingredientsdf[ingredientsdf['IngredListID'] == id].index) #delete the old ingredients
     for item in newIngredients:
-        item.update({"IngredListID":id}) #modify the existing dictionary to contain the existing id number that is associated with recipe and steps
-    newIngreddf = pd.DataFrame(columns = columnsIngredients, data = newIngredients) #create a df of newIngredients
-    ingredientsdf = pd.concat([ingredientsdf, newIngreddf], ignore_index = True) #save to the ingredientsdf 
-    save_dfs() 
+        item.update({"IngredListID":id})
+    newIngreddf = pd.DataFrame(columns = columnsIngredients, data = newIngredients)
+    ingredientsdf = pd.concat([ingredientsdf, newIngreddf], ignore_index = True) #save to the ingredientsdf
+    save_dfs()
     return
+
+def getTags():
+    global recipedf
+    unique_tags = recipedf['tags'].unique().tolist()
+    unique_foodCat = recipedf['foodCat'].unique().tolist()
+    unique_cuisine = recipedf['cuisine'].unique().tolist()
+    all_tags = unique_tags + unique_foodCat + unique_cuisine
+    all_tags = list(filter(("").__ne__, all_tags)) 
+    tags_filter = {}
+    tags_filter.update({"All":recipedf['RecipeName'].tolist()})
+    for tag in all_tags:
+        resultsList = []
+
+        resultsTags = recipedf.loc[recipedf["tags"] == tag]
+        resultsTags = resultsTags['RecipeName'].tolist()
+        if len(resultsTags) != 0 :
+            resultsList.extend(resultsTags)
+        resultsCuisine = recipedf.loc[recipedf["cuisine"] == tag]
+        resultsCuisine = resultsCuisine['RecipeName'].tolist()
+        if len(resultsCuisine) != 0 :
+            resultsList.extend(resultsCuisine)   
+
+        resultsFoodCat = recipedf.loc[recipedf["foodCat"] == tag]
+        resultsFoodCat = resultsFoodCat['RecipeName'].tolist()
+        if len(resultsFoodCat) != 0 :
+            resultsList.extend(resultsFoodCat)  
+
+        tags_filter.update({tag:resultsList})
+    return tags_filter
+
+def getLastRecipe():
+    #get the recipe associated with the  recipe name
+    global ingredientsdf, recipedf, stepsdf, filename #use global variables
+    recipes = recipedf['RecipeName'].tolist()
+    recipeName = recipes[-1]
+    return recipeName
 
 ############################################################################################################
 #
